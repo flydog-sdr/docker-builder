90b21830c9fa83d6e620f6e5ec5199868dcce36d
bclswl0827 <24319042+bclswl0827@users.noreply.github.com>
2020-11-21 20:59:50 +0800

    More functions

--- main/entrypoint.sh
+++ main/entrypoint.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
+# Bind IP Address of p.sdrotg.com
+echo "1.0.0.1 p.sdrotg.com" >> /etc/hosts
+
+# Check EEPROM to confirm device
+echo "24c32 0x54" > /sys/class/i2c-adapter/i2c-1/new_device
+
+if [ "$(/usr/bin/xxd /sys/bus/i2c/devices/1-0054/eeprom | grep -q -s "466c 7964 6f67" && echo true)" = "true" ] ; then
+    echo -e "\033[32mFlyDog SDR is detected, will now start the main process.\033[0m"
+else
+    echo -e "\033[31mFor compatibility reasons, if the device is not FlyDog SDR, the process will be exited with code 255.\033[0m"
+    exit 255
+fi
+
 # Start fan
+/usr/bin/gpio mode 26 pwm
+/usr/bin/gpio pwm 26 1024
 
-gpio mode 26 pwm
-gpio pwm 26 1024
+# Scaling CPU minial frequency
+echo 1000000 > /sys/devices/system/cpu/cpufreq/policy0/scaling_min_freq
 
-# Main process
+# Start main process
 /usr/local/bin/kiwid -debian 10 -use_spidev 1 -bg -raspsdr
