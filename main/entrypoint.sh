67ab2f9c3f49df08f87047b8bdaae3f30de5e90e
bclswl0827 <24319042+bclswl0827@users.noreply.github.com>
2020-11-21 22:01:10 +0800

    EEPROM detection

--- main/entrypoint.sh
+++ main/entrypoint.sh
@@ -4,14 +4,25 @@
 echo "1.0.0.1 p.sdrotg.com" >> /etc/hosts
 
 # Check EEPROM to confirm device
-echo "24c32 0x54" > /sys/class/i2c-adapter/i2c-1/new_device
-
-if [ "$(/usr/bin/xxd /sys/bus/i2c/devices/1-0054/eeprom | grep -q -s "466c 7964 6f67" && echo true)" = "true" ] ; then
-    echo -e "\033[32mFlyDog SDR is detected, will now start the main process.\033[0m"
+if [ ! -f "/sys/bus/i2c/devices/1-0054/eeprom" ];then
+    echo "24c32 0x54" > /sys/class/i2c-adapter/i2c-1/new_device
+    if [ "$(/usr/bin/xxd /sys/bus/i2c/devices/1-0054/eeprom | grep -q -s "466c 7964 6f67" && echo true)" = "true" ] ; then
+        echo -e "\033[32mFlyDog SDR is detected, will now start the main process.\033[0m"
+    else
+        echo -e "\033[31mFor compatibility reasons, if the device is not FlyDog SDR, the process will be exited with code 255.\033[0m"
+        echo "0x54" > /sys/class/i2c-adapter/i2c-1/delete_device
+        exit 255
+    fi
 else
-    echo -e "\033[31mFor compatibility reasons, if the device is not FlyDog SDR, the process will be exited with code 255.\033[0m"
-    exit 255
+    if [ "$(/usr/bin/xxd /sys/bus/i2c/devices/1-0054/eeprom | grep -q -s "466c 7964 6f67" && echo true)" = "true" ] ; then
+        echo -e "\033[32mFlyDog SDR is detected, will now start the main process.\033[0m"
+    else
+        echo -e "\033[31mFor compatibility reasons, if the device is not FlyDog SDR, the process will be exited with code 255.\033[0m"
+        echo "0x54" > /sys/class/i2c-adapter/i2c-1/delete_device
+        exit 255
+    fi
 fi
+echo "0x54" > /sys/class/i2c-adapter/i2c-1/delete_device
 
 # Start fan
 /usr/bin/gpio mode 26 pwm
